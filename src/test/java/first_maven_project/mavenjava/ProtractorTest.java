a1fe0cfc7109c42514aa2df124f353b7fca873e0
rasmi nayak <rasmi.kanta19@gmail.com>
2020-06-14 14:18:06 -0500

    Final Commit By X team

---
+++ src/test/java/first_maven_project/mavenjava/ProtractorTest.java
@@ -0,0 +1,13 @@
+package first_maven_project.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class ProtractorTest {
+	
+	@Test
+	public void Chrome()
+	{
+	System.out.println("Chrome");	
+	}
+
+}
